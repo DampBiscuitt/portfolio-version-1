e97d979d77fbe90c0f18ebe1965f80dbd1a31351
ST <159541338+DampBiscuitt@users.noreply.github.com>
2025-02-19 09:15:20 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-version-1/",
   css: {
     mofules: {
       localsConvention: 'camelCase'
